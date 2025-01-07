40c0b6989da1a22b7aa8320e8141546d018b93df
Alex <alexman900@gmail.com>
2025-01-07 12:38:44 -0600

    Update BasicTest.cpp

--- BasicTest.cpp
+++ BasicTest.cpp
@@ -1,1 +1,16 @@
-
+#include <string>
+#include <fstream>
+#include <vector>
+#include <utility> // std::pair
+#include <stdexcept> // std::runtime_error
+#include <sstream> // std::stringstream
+int main(int argc, char *argv[]) {
+    int size = sizeOf(argv)
+    if (size is == 2)
+    {
+        string filename = str(arv[2])
+        cout << filename << endl;
+    } 
+    
+    return 0;
+}
