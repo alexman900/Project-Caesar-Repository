c4578f89d23c9a5c06cdf754642f9a60885cacc1
Alex <alexman900@gmail.com>
2025-01-13 13:45:04 -0600

    Update CSV_Convert.py
    Added Command Line Parsing Class

--- CSV_Convert.py
+++ CSV_Convert.py
@@ -1,6 +1,8 @@
 import csv
 import sys
 import requests
+import os.path
+
 
 def read_csv(csvName,apiCred):
 	# opening the CSV file 
@@ -139,7 +141,93 @@ def readMultiple_csv(csvNames,credentials):
 			data.extend(tempdata)
 		
 	return data
+
+
+
+
+
+def parseCommandLine(argv):
+	templist = list(argv)
+	print(templist)
+	del templist[0]
+	APICredential = "empty"
+	returnList = []
+	filelist = [];
+	if len(templist) == 1:
+		if (str(templist[0]) == "-h"):
+			printHelpMethod()			
 	
+			del templist[0]
+		else:
+			print("Error ", templist[0], " is not a valid argument /n Please use -h to get the list of valid arguments")
+			templist.clear()
+
+	while len(templist) >= 2:
+		if (str(templist[0]) == "-f"):
+			if os.path.isfile((str(templist[1]) + ".csv")):
+				filelist.append(str(templist[1]))
+				print((str(templist[1]) + ".csv"), " exists")
+				del templist[0]
+				del templist[0]
+			else:
+				print((str(templist[1]) + ".csv"), " does not exist")
+				del templist[0]
+				del templist[0]
+
+		elif (str(templist[0]) == "-fs"):
+			if int(templist[1]) >=1:
+				FileCount = (int(templist[1]))
+				i = 0;
+				while i < FileCount:
+					if os.path.isfile((str(templist[i+2]) + ".csv")):
+						print((str(templist[i+2]) + ".csv"), " exists")
+						filelist.append(str(templist[i+2]))
+						i = i+1
+					else:
+						print((str(templist[i+2]) + ".csv"), " does not exist")
+						i = i+1
+				i = 0;
+				while i < FileCount:
+					del templist[2]
+					i = i+1
+				del templist[0]
+				del templist[0]
+			else:
+				print("error 2nd arg in -fs was not a number greater than 0")				
+				templist.clear()
+							
+
+		elif (str(templist[0]) == "-h"):
+			printHelpMethod()
+			del templist[0]
+
+		elif (str(templist[0]) == "-c"):
+			#do something
+			print("APICRED Should be: ", str(templist[1]))
+			APICredential = str(templist[1])
+			del templist[0]
+			del templist[0]
+		else:
+			print("Error ", templist[0], " is not a valid argument /n Please use -h to get the list of valid arguments")
+			templist.clear()
+	if APICredential == "empty":
+		returnList.append(filelist)
+
+	else:
+		returnList.append(filelist)
+		returnList.append(APICredential)	
+		
+	return returnList
+	
+def printHelpMethod():
+	print("Usage: \n")
+	print("\t python3 CSV_Convert.py [OPTIONS]")
+	print("\n")
+	print("Argument Options: \n")
+	print("\t -f [filename]\t\t Convert a Single File")
+	print("\t -fs [Number of Files] [filename] [filename] [filename] ...\t\t Convert a Multiple File Please leave out .csv")
+	print("\t -c [API Credential]\t\t Define an API Credential besides the default")
+	print("\t -h help method")
 
 # main class to
 def main():
